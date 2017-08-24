c5021ba9ec7765029ab867c1363476942b36d05e
Jesse Millar <hellojessemillar@gmail.com>
2017-08-24 10:39:20 -0500

    Working code

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by j0m01o9 on 8/24/17.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
