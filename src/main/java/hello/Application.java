319394affe83685a32f17d8edcd18fc8b46ab359
Jesse Millar <hellojessemillar@gmail.com>
2017-08-24 11:29:54 -0500

    Working with Tomcat

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,14 +2,22 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by j0m01o9 on 8/24/17.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
   }
+
 }
