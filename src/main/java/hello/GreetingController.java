02dcfa681c576506f4a2ee8f72d884f36125b652
Jesse Millar <hellojessemillar@gmail.com>
2017-11-29 13:32:01 -0600

    A small change to make things more explicit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class GreetingController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @RequestMapping("/greeting")
+  @RequestMapping(value = "/greeting", method = RequestMethod.GET)
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     log.error("Returning response");
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
